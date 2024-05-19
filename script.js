b4f69c3a277c903e1942dd5d1c179dc7462708e6
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-20 01:03:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	if (s.length<0){
+		return s;
+	}
+	s.trim();
+	let p="";
+	for(let i in s){
+		if(s.charAt(i)==' '){
+			p=s.slice(0,i);
+			break;
+		}
+	}
+	return p;
 }
 
 // Do not change the code below
